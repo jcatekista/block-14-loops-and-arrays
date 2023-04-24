1f357eb5481bd8c7d58e68efdf044acfca6f63db
jcatekista <jcatekista@gmail.com>
2023-04-23 23:59:33 -0700

    updated fizzbuzz code

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,13 +1,16 @@
+//print 1-100
 let i;
-
+//variables 1-100
 for (i = 1; i < 100; i++) {
+  //number divisible by 3 and 5? print 'FizzBuzz'
+  //in place of number
   if (i % 15 == 0) console.log("FizzBuzz");
   // number divisible by 3? print 'Fizz'
-  // in place of the number
+  // in place of number
   else if (i % 3 == 0) console.log("Fizz");
   // number divisible by 5, print 'Buzz'
-  // in place of the number
+  // in place of number
   else if (i % 5 == 0) console.log("Buzz");
-  // print the number
+  // print number
   else console.log(i);
 }
---
+++ vowel-vs-consonant.js
@@ -0,0 +1,20 @@
+function vowel_count(str1) {
+  var vowel_list = "aeiouAEIOU";
+  var vcount = 0;
+
+  for (var x = 0; x < str1.length; x++) {
+    if (vowel_list.indexOf(str1[x]) !== -1) {
+      vcount += 1;
+    }
+  }
+  return vcount;
+}
+console.log(
+  str1 +
+    "has " +
+    " " +
+    "consonants and " +
+    vowel_count("The quick brown fox") +
+    " " +
+    "vowels"
+);
