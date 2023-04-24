f0d3b2070f3bc3039578b29fce9a4fe89583089c
jcatekista <jocateko@SEA-GP3N6GQ9XFN.ant.amazon.com>
2023-04-24 11:08:37 -0700

    cleaned up fizzbuzz code and finished vowels code

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,13 +3,10 @@ let i;
 //variables 1-100
 for (i = 1; i < 100; i++) {
   //number divisible by 3 and 5? print 'FizzBuzz'
-  //in place of number
   if (i % 15 == 0) console.log("FizzBuzz");
   // number divisible by 3? print 'Fizz'
-  // in place of number
   else if (i % 3 == 0) console.log("Fizz");
   // number divisible by 5, print 'Buzz'
-  // in place of number
   else if (i % 5 == 0) console.log("Buzz");
   // print number
   else console.log(i);
--- vowel-vs-consonant.js
+++ vowel-vs-consonant.js
@@ -1,20 +1,134 @@
-function vowel_count(str1) {
-  var vowel_list = "aeiouAEIOU";
-  var vcount = 0;
-
-  for (var x = 0; x < str1.length; x++) {
-    if (vowel_list.indexOf(str1[x]) !== -1) {
-      vcount += 1;
-    }
+//set string or word to check
+let string = "hello";
+//define count for vowels and consonants
+let vowels = 0;
+let consonants = 0;
+//for loop code to scan string  or word length
+for (let i = 0; i < string.length; i++) {
+  let char = string[i].toLowerCase();
+  //if any characters are a,e,i,o,u, count as vowels
+  if (
+    char === "a" ||
+    char === "e" ||
+    char === "i" ||
+    char === "o" ||
+    char === "u"
+  ) {
+    vowels++;
+    //else count all other letters as consonants
+  } else if (char >= "a" && char <= "z") {
+    consonants++;
   }
-  return vcount;
 }
+//print string with counts of vowels and consonants
 console.log(
-  str1 +
-    "has " +
-    " " +
-    "consonants and " +
-    vowel_count("The quick brown fox") +
-    " " +
-    "vowels"
+  string + " has " + consonants + " consonants and " + vowels + " vowels."
+);
+
+//set string or word to check
+let string1 = "ukulele";
+//define count for vowels and consonants
+let vowels1 = 0;
+let consonants1 = 0;
+//for loop code to scan string  or word length
+for (let i = 0; i < string1.length; i++) {
+  let char = string1[i].toLowerCase();
+  //if any characters are a,e,i,o,u, count as vowels
+  if (
+    char === "a" ||
+    char === "e" ||
+    char === "i" ||
+    char === "o" ||
+    char === "u"
+  ) {
+    vowels1++;
+    //else count all other letters as consonants
+  } else if (char >= "a" && char <= "z") {
+    consonants1++;
+  }
+}
+//print string with counts of vowels and consonants
+console.log(
+  string1 + " has " + consonants1 + " consonants and " + vowels1 + " vowels."
+);
+
+//set string or word to check
+let string2 = "awesome";
+//define count for vowels and consonants
+let vowels2 = 0;
+let consonants2 = 0;
+//for loop code to scan string  or word length
+for (let i = 0; i < string2.length; i++) {
+  let char = string2[i].toLowerCase();
+  //if any characters are a,e,i,o,u, count as vowels
+  if (
+    char === "a" ||
+    char === "e" ||
+    char === "i" ||
+    char === "o" ||
+    char === "u"
+  ) {
+    vowels2++;
+    //else count all other letters as consonants
+  } else if (char >= "a" && char <= "z") {
+    consonants2++;
+  }
+}
+//print string with counts of vowels and consonants
+console.log(
+  string2 + " has " + consonants2 + " consonants and " + vowels2 + " vowels."
+);
+
+//set string or word to check
+let string3 = "onomonopia";
+//define count for vowels and consonants
+let vowels3 = 0;
+let consonants3 = 0;
+//for loop code to scan string  or word length
+for (let i = 0; i < string3.length; i++) {
+  let char = string3[i].toLowerCase();
+  //if any characters are a,e,i,o,u, count as vowels
+  if (
+    char === "a" ||
+    char === "e" ||
+    char === "i" ||
+    char === "o" ||
+    char === "u"
+  ) {
+    vowels3++;
+    //else count all other letters as consonants
+  } else if (char >= "a" && char <= "z") {
+    consonants3++;
+  }
+}
+//print string with counts of vowels and consonants
+console.log(
+  string3 + " has " + consonants3 + " consonants and " + vowels3 + " vowels."
+);
+
+//set string or word to check
+let string4 = "textbook";
+//define count for vowels and consonants
+let vowels4 = 0;
+let consonants4 = 0;
+//for loop code to scan string  or word length
+for (let i = 0; i < string4.length; i++) {
+  let char = string4[i].toLowerCase();
+  //if any characters are a,e,i,o,u, count as vowels
+  if (
+    char === "a" ||
+    char === "e" ||
+    char === "i" ||
+    char === "o" ||
+    char === "u"
+  ) {
+    vowels4++;
+    //else count all other letters as consonants
+  } else if (char >= "a" && char <= "z") {
+    consonants4++;
+  }
+}
+//print string with counts of vowels and consonants
+console.log(
+  string4 + " has " + consonants4 + " consonants and " + vowels4 + " vowels."
 );
